ff07800293d3ec919b6972c9cda6eecaaf043ff2
Andrei Bo <andreibo@AUGH.local>
2025-03-13 17:59:17 -0700

    Updated and changed my idea. It will now be a spiderman idle animation. Finished creating base of spiderman

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -33,6 +33,9 @@ var modelViewMatrixLoc, projectionMatrixLoc, normalMatrixLoc;
 var eye;
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
+var cameraAngle = Math.PI/2;
+var rotationSpeed = 1.0;
+
 
 var RX = 0;
 var RY = 0;
@@ -43,50 +46,24 @@ var TIME = 0.0; // Realtime
 var dt = 0.0
 var prevTime = 0.0;
 var resetTimerFlag = true;
-
-
+var animFlag = false;
+var controller;
 
 // These are used to store the current state of objects.
 // In animation it is often useful to think of an object as having some DOF
-// Then the animation is simply evolving those DOF over time.
-var currentRotation = [0,0,0];
+// Then the animation is simply evolving those DOF over time. You could very easily make a higher level object that stores these as Position, Rotation (and also Scale!)
+var sphereRotation = [0,0,0];
+var spherePosition = [-4,0,0];
 
-var useTextures = 1;
+var cubeRotation = [0,0,0];
+var cubePosition = [0,0,0];
 
-//making a texture image procedurally
-//Let's start with a 1-D array
-var texSize = 25;
-var imageCheckerBoardData = new Array();
+var cylinderRotation = [0,0,0];
+var cylinderPosition = [1.1,0,0];
 
-// Now for each entry of the array make another array
-// 2D array now!
-for (var i =0; i<texSize; i++)
-	imageCheckerBoardData[i] = new Array();
+var coneRotation = [0,0,0];
+var conePosition = [3,0,0];
 
-// Now for each entry in the 2D array make a 4 element array (RGBA! for colour)
-for (var i =0; i<texSize; i++)
-	for ( var j = 0; j < texSize; j++)
-		imageCheckerBoardData[i][j] = new Float32Array(4);
-
-// Now for each entry in the 2D array let's set the colour.
-// We could have just as easily done this in the previous loop actually
-for (var i =0; i<texSize; i++) 
-	for (var j=0; j<texSize; j++) {
-		var c = (i + j ) % 2;
-		imageCheckerBoardData[i][j] = [c, c, c, 1];
-}
-
-//Convert the image to uint8 rather than float.
-var imageCheckerboard = new Uint8Array(4*texSize*texSize);
-
-for (var i = 0; i < texSize; i++)
-	for (var j = 0; j < texSize; j++)
-	   for(var k =0; k<4; k++)
-			imageCheckerboard[4*texSize*i+4*j+k] = 255*imageCheckerBoardData[i][j][k];
-		
-// For this example we are going to store a few different textures here
-var textureArray = [] ;
-    
 // Setting the colour which is needed during illumination of a surface
 function setColor(c)
 {
@@ -101,182 +78,9 @@ function setColor(c)
     gl.uniform4fv( gl.getUniformLocation(program,
                                          "specularProduct"),flatten(specularProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,
-                                         "lightPosition"),flatten(lightPosition2) );
+                                         "lightPosition"),flatten(lightPosition) );
     gl.uniform1f( gl.getUniformLocation(program, 
                                         "shininess"),materialShininess );
-}
-
-// We are going to asynchronously load actual image files this will check if that call if an async call is complete
-// You can use this for debugging
-function isLoaded(im) {
-    if (im.complete) {
-        console.log("loaded") ;
-        return true ;
-    }
-    else {
-        console.log("still not loaded!!!!") ;
-        return false ;
-    }
-}
-
-// Helper function to load an actual file as a texture
-// NOTE: The image is going to be loaded asyncronously (lazy) which could be
-// after the program continues to the next functions. OUCH!
-function loadFileTexture(tex, filename)
-{
-	//create and initalize a webgl texture object.
-    tex.textureWebGL  = gl.createTexture();
-    tex.image = new Image();
-    tex.image.src = filename ;
-    tex.isTextureReady = false ;
-    tex.image.onload = function() { handleTextureLoaded(tex); }
-}
-
-// Once the above image file loaded with loadFileTexture is actually loaded,
-// this funcion is the onload handler and will be called.
-function handleTextureLoaded(textureObj) {
-	//Binds a texture to a target. Target is then used in future calls.
-		//Targets:
-			// TEXTURE_2D           - A two-dimensional texture.
-			// TEXTURE_CUBE_MAP     - A cube-mapped texture.
-			// TEXTURE_3D           - A three-dimensional texture.
-			// TEXTURE_2D_ARRAY     - A two-dimensional array texture.
-    gl.bindTexture(gl.TEXTURE_2D, textureObj.textureWebGL);
-	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); // otherwise the image would be flipped upsdide down
-	
-	//texImage2D(Target, internalformat, width, height, border, format, type, ImageData source)
-    //Internal Format: What type of format is the data in? We are using a vec4 with format [r,g,b,a].
-        //Other formats: RGB, LUMINANCE_ALPHA, LUMINANCE, ALPHA
-    //Border: Width of image border. Adds padding.
-    //Format: Similar to Internal format. But this responds to the texel data, or what kind of data the shader gets.
-    //Type: Data type of the texel data
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureObj.image);
-	
-	//Set texture parameters.
-    //texParameteri(GLenum target, GLenum pname, GLint param);
-    //pname: Texture parameter to set.
-        // TEXTURE_MAG_FILTER : Texture Magnification Filter. What happens when you zoom into the texture
-        // TEXTURE_MIN_FILTER : Texture minification filter. What happens when you zoom out of the texture
-    //param: What to set it to.
-        //For the Mag Filter: gl.LINEAR (default value), gl.NEAREST
-        //For the Min Filter: 
-            //gl.LINEAR, gl.NEAREST, gl.NEAREST_MIPMAP_NEAREST, gl.LINEAR_MIPMAP_NEAREST, gl.NEAREST_MIPMAP_LINEAR (default value), gl.LINEAR_MIPMAP_LINEAR.
-    //Full list at: https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/texParameter
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_NEAREST);
-	
-	//Generates a set of mipmaps for the texture object.
-        /*
-            Mipmaps are used to create distance with objects. 
-        A higher-resolution mipmap is used for objects that are closer, 
-        and a lower-resolution mipmap is used for objects that are farther away. 
-        It starts with the resolution of the texture image and halves the resolution 
-        until a 1x1 dimension texture image is created.
-        */
-    gl.generateMipmap(gl.TEXTURE_2D);
-	
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); //Prevents s-coordinate wrapping (repeating)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); //Prevents t-coordinate wrapping (repeating)
-    gl.bindTexture(gl.TEXTURE_2D, null);
-    console.log(textureObj.image.src) ;
-    
-    textureObj.isTextureReady = true ;
-}
-
-// Takes an array of textures and calls render if the textures are created/loaded
-// This is useful if you have a bunch of textures, to ensure that those files are
-// actually loaded from disk you can wait and delay the render function call
-// Notice how we call this at the end of init instead of just calling requestAnimFrame like before
-function waitForTextures(texs) {
-    setTimeout(
-		function() {
-			   var n = 0 ;
-               for ( var i = 0 ; i < texs.length ; i++ )
-               {
-                    console.log(texs[i].image.src) ;
-                    n = n+texs[i].isTextureReady ;
-               }
-               wtime = (new Date()).getTime() ;
-               if( n != texs.length )
-               {
-               		console.log(wtime + " not ready yet") ;
-               		waitForTextures(texs) ;
-               }
-               else
-               {
-               		console.log("ready to render") ;
-					render(0);
-               }
-		},
-	5) ;
-}
-
-// This will use an array of existing image data to load and set parameters for a texture
-// We'll use this function for procedural textures, since there is no async loading to deal with
-function loadImageTexture(tex, image) {
-	//create and initalize a webgl texture object.
-    tex.textureWebGL  = gl.createTexture();
-    tex.image = new Image();
-
-	//Binds a texture to a target. Target is then used in future calls.
-		//Targets:
-			// TEXTURE_2D           - A two-dimensional texture.
-			// TEXTURE_CUBE_MAP     - A cube-mapped texture.
-			// TEXTURE_3D           - A three-dimensional texture.
-			// TEXTURE_2D_ARRAY     - A two-dimensional array texture.
-    gl.bindTexture(gl.TEXTURE_2D, tex.textureWebGL);
-
-	//texImage2D(Target, internalformat, width, height, border, format, type, ImageData source)
-    //Internal Format: What type of format is the data in? We are using a vec4 with format [r,g,b,a].
-        //Other formats: RGB, LUMINANCE_ALPHA, LUMINANCE, ALPHA
-    //Border: Width of image border. Adds padding.
-    //Format: Similar to Internal format. But this responds to the texel data, or what kind of data the shader gets.
-    //Type: Data type of the texel data
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
-	
-	//Generates a set of mipmaps for the texture object.
-        /*
-            Mipmaps are used to create distance with objects. 
-        A higher-resolution mipmap is used for objects that are closer, 
-        and a lower-resolution mipmap is used for objects that are farther away. 
-        It starts with the resolution of the texture image and halves the resolution 
-        until a 1x1 dimension texture image is created.
-        */
-    gl.generateMipmap(gl.TEXTURE_2D);
-	
-	//Set texture parameters.
-    //texParameteri(GLenum target, GLenum pname, GLint param);
-    //pname: Texture parameter to set.
-        // TEXTURE_MAG_FILTER : Texture Magnification Filter. What happens when you zoom into the texture
-        // TEXTURE_MIN_FILTER : Texture minification filter. What happens when you zoom out of the texture
-    //param: What to set it to.
-        //For the Mag Filter: gl.LINEAR (default value), gl.NEAREST
-        //For the Min Filter: 
-            //gl.LINEAR, gl.NEAREST, gl.NEAREST_MIPMAP_NEAREST, gl.LINEAR_MIPMAP_NEAREST, gl.NEAREST_MIPMAP_LINEAR (default value), gl.LINEAR_MIPMAP_LINEAR.
-    //Full list at: https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/texParameter
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); //Prevents s-coordinate wrapping (repeating)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); //Prevents t-coordinate wrapping (repeating)
-    gl.bindTexture(gl.TEXTURE_2D, null);
-
-    tex.isTextureReady = true;
-}
-
-// This just calls the appropriate texture loads for this example adn puts the textures in an array
-function initTexturesForExample() {
-    textureArray.push({}) ;
-    loadFileTexture(textureArray[textureArray.length-1],"box.png") ;
-    
-    textureArray.push({}) ;
-    loadImageTexture(textureArray[textureArray.length-1],imageCheckerboard) ;
-}
-
-// Changes which texture is active in the array of texture examples (see initTexturesForExample)
-function toggleTextures() {
-    useTextures = (useTextures + 1) % 2
-	gl.uniform1i(gl.getUniformLocation(program, "useTextures"), useTextures);
 }
 
 window.onload = function init() {
@@ -323,16 +127,22 @@ window.onload = function init() {
        "lightPosition"),flatten(lightPosition) );
     gl.uniform1f( gl.getUniformLocation(program, 
        "shininess"),materialShininess );
-    
-    document.getElementById("textureToggleButton").onclick = function() {
-        toggleTextures() ;
-        window.requestAnimFrame(render);
+
+
+    document.getElementById("animToggleButton").onclick = function() {
+        if( animFlag ) {
+            animFlag = false;
+        }
+        else {
+            animFlag = true;
+            resetTimerFlag = true;
+            cameraAngle = Math.PI/2;
+            window.requestAnimFrame(render);
+        }
+        //console.log(animFlag);
     };
 
-	// Helper function just for this example to load the set of textures
-    initTexturesForExample() ;
-
-    waitForTextures(textureArray);
+    render(0);
 }
 
 // Sets the modelview and normal matrix in the shaders
@@ -384,19 +194,19 @@ function drawCone() {
 }
 
 // Post multiples the modelview matrix with a translation matrix
-// and replaces the modeling matrix with the result
+// and replaces the modeling matrix with the result, x, y, and z are the translation amounts for each axis
 function gTranslate(x,y,z) {
     modelMatrix = mult(modelMatrix,translate([x,y,z]));
 }
 
 // Post multiples the modelview matrix with a rotation matrix
-// and replaces the modeling matrix with the result
+// and replaces the modeling matrix with the result, theta is the rotation amount, x, y, z are the components of an axis vector (angle, axis rotations!)
 function gRotate(theta,x,y,z) {
     modelMatrix = mult(modelMatrix,rotate(theta,[x,y,z]));
 }
 
 // Post multiples the modelview matrix with a scaling matrix
-// and replaces the modeling matrix with the result
+// and replaces the modeling matrix with the result, x, y, and z are the scale amounts for each axis
 function gScale(sx,sy,sz) {
     modelMatrix = mult(modelMatrix,scale(sx,sy,sz));
 }
@@ -412,68 +222,133 @@ function gPush() {
 }
 
 
+
+
+
 function render(timestamp) {
     
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    if (animFlag) {
+        cameraAngle += rotationSpeed * dt;
+        cameraAngle %= (2 * Math.PI);
+    }
     
-    eye = vec3(0,-10,10);
+    var radius = 30.0;
+    eye = vec3(
+        radius * Math.cos(cameraAngle),
+        1,
+        radius * Math.sin(cameraAngle)
+    );
     MS = []; // Initialize modeling matrix stack
 	
 	// initialize the modeling matrix to identity
     modelMatrix = mat4();
     
     // set the camera matrix
-    viewMatrix = lookAt(eye, at, up);
+    viewMatrix = lookAt(eye, at , up);
    
     // set the projection matrix
     projectionMatrix = ortho(left, right, bottom, ytop, near, far);
-
+    
     
     // set all the matrices
     setAllMatrices();
     
-	// dt is the change in time or delta time from the last frame to this one
-	// in animation typically we have some property or degree of freedom we want to evolve over time
-	// For example imagine x is the position of a thing.
-	// To get the new position of a thing we do something called integration
-	// the simpelst form of this looks like:
-	// x_new = x + v*dt
-	// That is the new position equals the current position + the rate of of change of that position (often a velocity or speed), times the change in time
-	// We can do this with angles or positions, the whole x,y,z position or just one dimension. It is up to us!
-	dt = (timestamp - prevTime) / 1000.0;
-	prevTime = timestamp;
-	
-	// We need to bind our textures, ensure the right one is active before we draw
-	//Activate a specified "texture unit".
-    //Texture units are of form gl.TEXTUREi | where i is an integer.
-	gl.activeTexture(gl.TEXTURE0);
-	if (useTextures % 2 == 1) 
-	{
-		//Binds a texture to a target. Target is then used in future calls.
-		//Targets:
-			// TEXTURE_2D           - A two-dimensional texture.
-			// TEXTURE_CUBE_MAP     - A cube-mapped texture.
-			// TEXTURE_3D           - A three-dimensional texture.
-			// TEXTURE_2D_ARRAY     - A two-dimensional array texture.
-		gl.bindTexture(gl.TEXTURE_2D, textureArray[0].textureWebGL);
-		gl.uniform1i(gl.getUniformLocation(program, "texture1"), 0);
-	}
-    else
-	{
-		gl.bindTexture(gl.TEXTURE_2D, textureArray[1].textureWebGL);
-		gl.uniform1i(gl.getUniformLocation(program, "texture2"), 0);
+	if( animFlag )
+    {
+		// dt is the change in time or delta time from the last frame to this one
+		// in animation typically we have some property or degree of freedom we want to evolve over time
+		// For example imagine x is the position of a thing.
+		// To get the new position of a thing we do something called integration
+		// the simpelst form of this looks like:
+		// x_new = x + v*dt
+		// That is, the new position equals the current position + the rate of of change of that position (often a velocity or speed) times the change in time
+		// We can do this with angles or positions, the whole x,y,z position, or just one dimension. It is up to us!
+		dt = (timestamp - prevTime) / 2000;
+		prevTime = timestamp;
 	}
 	
-	// Now let's draw a shape animated!
-	// You may be wondering where the texture coordinates are!
-	// We've modified the object.js to add in support for this attribute array!
+	// Platform
 	gPush();
-	{
-		currentRotation[2] = currentRotation[2] + 30*dt;
-		gRotate(currentRotation[2],0,0,1);
-		drawCube();
-	}
-	gPop() ;
-	
-    window.requestAnimFrame(render);
+		gTranslate(cubePosition[0],-4,cubePosition[2]);
+		gPush();
+		{
+			setColor(vec4(0.9,0.9,0.9,1.0));
+			// cubeRotation[1] = cubeRotation[1] + 30*dt;
+			// gRotate(cubeRotation[1],0,1,0);
+            gScale(40.0, 0.1, 40.0);
+			drawSphere();
+		}
+		gPop();
+	gPop();
+ 
+        
+         
+
+    // Spiderman
+    gPush();
+        gTranslate(0,2.5,0);
+        // Torso
+        gPush();
+        {
+            setColor(vec4(1.0,0.0,0.0,1.0));
+            gScale(1, 1.5, 0.5);
+            drawCube();
+        }
+        gPop();
+
+        // Head
+        gPush();
+        {
+            setColor(vec4(1.0,0.0,0.0,1.0));
+            gTranslate(0,2.5,0);
+            gScale(1, 1, 1);
+            drawCube();
+        }
+        gPop();
+
+        // Right Arm
+        gPush();
+        {
+            setColor(vec4(1.0,0.0,0.0,1.0));
+            gTranslate(-1.5, 0, 0);
+            gScale(0.5, 1.5, 0.5);
+            drawCube();
+        }
+        gPop();
+
+        // Left Arm
+        gPush();
+        {
+            setColor(vec4(1.0,0.0,0.0,1.0));
+            gTranslate(1.5, 0, 0);
+            gScale(0.5, 1.5, 0.5);
+            drawCube();
+        }
+        gPop();
+
+        // Right Leg
+        gPush();
+        {
+            setColor(vec4(1.0,0.0,0.0,1.0));
+            gTranslate(-0.5, -3, 0);
+            gScale(0.5, 1.5, 0.5);
+            drawCube();
+        }
+        gPop();        
+
+        // Left Leg
+        gPush();
+        {
+            setColor(vec4(1.0,0.0,0.0,1.0));
+            gTranslate(0.5, -3, 0);
+            gScale(0.5, 1.5, 0.5);
+            drawCube();
+        }
+        gPop();            
+
+    gPop();
+    if( animFlag )
+        window.requestAnimFrame(render);
 }
